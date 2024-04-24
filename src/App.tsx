40628c680c20384ddf34361c3988d47daa6cd1f6
SupakornS <67590299+SupakornSJB@users.noreply.github.com>
2024-04-24 13:41:27 +0700

    fix: Adapt to Screen Size

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useMemo, useState } from 'react'
 import './App.css'
 
 const allQuestion = [
-  "いま なに が いちばん ほしい ですか。(lesson 13) ",
+  "いま なに が いちばん ほしい ですか。",
   "しゅうまつ は なに を します か。",
   "ソンクランやすみ は どこか いきましたか。",
   "あついですね。エアコンを つけましょうか。",
   "(look at the picture) 〜さん は なに を していますか。",
   "いま、あめ が ふっていますか。",
-  "すみません。この カタカナ の よみかた を おしえてください。(lesson 14)",
+  "すみません。この カタカナ の よみかた を おしえてください。",
   "ここ に なまえ を かいてください。",
   "くるま を もっています か。",
   "ごかぞく は なんにん ですか。",
@@ -26,14 +26,14 @@ const allQuestion = [
   "〜さん は どんな ひと ですか。",
   "〜さん、ひるごはん を たべ に いきませんか。",
   "いらっしゃいませ！ごちゅうもん は？",
-  ". (after the meal, you want to pay the bill separately) (lesson 13) ３０００えん です。",
-  "we are friends. (lesson 13) のど が かわきましたね。",
+  ". (after the meal, you want to pay the bill separately) ３０００えん です。",
+  "we are friends. のど が かわきましたね。",
   "にもつ が おおい ですね。もちましょうか。",
   "go straight and turn left (right) at the traffic light. stop in the front of that flower shop.",
   "(at the immigration control) パスポートを みせてください。",
-  "すみません。ちょっと おかね を かしてください。(lesson 14)",
-  "You want to take a photo but don't know if you can do that. Ask for permission. (lesson 15) use a computer",
-  "You want to take a photo but don't know if you can do that. Ask for permission. (lesson 15) use a pen"
+  "すみません。ちょっと おかね を かしてください。",
+  "You want to take a photo but don't know if you can do that. Ask for permission. use a computer",
+  "You want to take a photo but don't know if you can do that. Ask for permission. use a pen"
 ]
 
 function App() {
@@ -43,7 +43,7 @@ function App() {
   return (
     <div className='w-full h-screen'>
       <div className="flex items-center justify-center h-full">
-        <div className="flex flex-col gap-3 border-2 rounded max-w-7xl w-2/5 p-10">
+        <div className="flex flex-col gap-3 border-2 rounded max-w-7xl w-5/6 lg:w-2/5 p-10 sm:w-4/5 ">
           <div className='font-bold text-3xl flex justify-center'>
             {randomWord}
           </div>
