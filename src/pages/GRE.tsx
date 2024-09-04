42e77c24c773873c36a22a600301baad25949131
SupakornSJB <supakorn.sen@outlook.com>
2024-09-04 19:12:49 +0700

    update: Change the UI for mobile

--- src/pages/GRE.tsx
+++ src/pages/GRE.tsx
@@ -22,23 +22,27 @@ export const GREPage: React.FC = () => {
 
   return (
     <>
-      <div className="card card-bordered border-white w-4/5 lg:w-1/2 border-2">
-        <div className="card-body flex justify-center items-center">
-          <p className="font-bold text-3xl">{current.question}</p>
-          {
-            isShowingAnswer ?
-              <div>
-                <span>{current.answer}</span>{" "}
-                <span className="font-bold">({current.setName})</span>
-              </div>
-              : <p className="text-error">Click "Show Answer" to reveal answer</p>
-          }
-          <div className="card-actions ">
-            <button className="btn btn-primary" onClick={handleClickNext}>
-              {isShowingAnswer ? "Next" : "Show Answer"}
-            </button>
-            <button className="btn btn-neutral" onClick={openSettingModal}>
+      <div className="card card-bordered border-neutral w-4/5 lg:w-1/2 h-1/2 border-2">
+        <div className="card-body p-4 flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center border-neutral-50 bg-neutral p-3 rounded-xl w-full h-2/3 grow">
+            <span className="font-bold text-3xl ">{current.question}</span>
+            <div className="pt-3">
+              {
+                isShowingAnswer ?
+                  <div className="text-center">
+                    <span className="">{current.answer}</span>{" "}
+                    <span className="font-bold">({current.setName})</span>
+                  </div>
+                  : <p className="text-error text-center">Click "Show Answer" to reveal answer</p>
+              }
+            </div>
+          </div>
+          <div className="p-0 sm:p-3 border-0 rounded-xl sm:border card-actions border-neutral flex sm:flex-row-reverse justify-center items-center w-full ">
+            <button className="btn btn-outline border-0 btn-sm w-full sm:w-auto" onClick={openSettingModal}>
               Option
+            </button>
+            <button className={`btn ${isShowingAnswer ? "btn-secondary" : "btn-primary"} btn-sm w-full sm:w-auto`} onClick={handleClickNext}>
+              {isShowingAnswer ? "Next Question" : "Show Answer"}
             </button>
           </div>
         </div>
